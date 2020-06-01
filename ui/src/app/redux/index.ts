0da58418dd575fb259a8b625620484a26fcbe214
Rajkumar <jeevanraorajaram@gmail.com>
2020-06-01 14:16:50 +0530

    delete

--- ui/src/app/redux/index.ts
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import LoginReducer from './reducers/login-reducer';
-import SessionReducer from './reducers/session-reducer';
-
-export const rootReducer = combineReducers({
-  sessionReducer: SessionReducer,
-  loginReducer: LoginReducer,
-});
