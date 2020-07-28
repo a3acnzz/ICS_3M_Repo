79871de13152b170b1b1a30e5c92cbc8b9750dc2
Rajkumar <jeevanraorajaram@gmail.com>
2020-07-28 16:24:47 +0530

    ics prod and qa move

--- ICS - latest source code/api/grails-app/services/com/mmm/AnnouncementService.groovy
+++ ICS - latest source code/api/grails-app/services/com/mmm/AnnouncementService.groovy
@@ -8,52 +8,58 @@ import grails.transaction.Transactional
 @Transactional
 class AnnouncementService {
 
-    SpringSecurityService springSecurityService
-    ProgramOwnerService programOwnerService
+	SpringSecurityService springSecurityService
+	ProgramOwnerService programOwnerService
 
-    @Transactional(readOnly = true)
-    def search(Long locationId, Boolean active) {
-        def results = Announcement.createCriteria().list() {
+	@Transactional(readOnly = true)
+	def search(Long locationId, Boolean active) {
+		def results = Announcement.createCriteria().list() {
 
-            if (locationId != null) {
-                eq("location.id", locationId)
-            } else {
-                isNull("location.id")
-            }
-            if (active != null) {
-                eq("active", active)
-            }
-        }
-        return results
-    }
+			if (locationId != null) {
+				eq("location.id", locationId)
+			} else {
+				isNull("location.id")
+			}
+			if (active != null) {
+				eq("active", active)
+			}
+		}
+		return results
+	}
 
-    @Transactional
-    def save(def json) {
-        // Check that the program owner is authorized for the submitted location
-        if (!programOwnerService.programOwnerAuthorized(json.location.id)) {
-            throw new UnauthorizedException()
-        }
+	@Transactional
+	def save(def json) {
+		// Check that the program owner is authorized for the submitted location
+		if (json.id) {
+			if (!programOwnerService.programOwnerAuthorized(json.location.id)) {
+				throw new UnauthorizedException()
+			}
+		} else {
+			if (!programOwnerService.programOwnerAuthorized(json.locationId)) {
+				throw new UnauthorizedException()
+			}
+		}
 
-        CustomUserDetail user = springSecurityService?.principal
+		CustomUserDetail user = springSecurityService?.principal
 
-        Announcement submittedAnnouncement = new Announcement(json)
-        Announcement announcement
+		Announcement submittedAnnouncement = new Announcement(json)
+		Announcement announcement
 
-        if (json.locationId) {
-            submittedAnnouncement.location = Location.load(json.locationId)
-        }
+		if (json.locationId) {
+			submittedAnnouncement.location = Location.load(json.locationId)
+		}
 
-        if (json.id) {
-            announcement = Announcement.get(json.id)
-        }
-        if (!announcement) {
-            announcement = new Announcement()
-            announcement.createdBy = user?.username
-        }
-        announcement.lastUpdatedBy = user?.username
-        announcement.copy(submittedAnnouncement)
-        announcement.save()
+		if (json.id) {
+			announcement = Announcement.get(json.id)
+		}
+		if (!announcement) {
+			announcement = new Announcement()
+			announcement.createdBy = user?.username
+		}
+		announcement.lastUpdatedBy = user?.username
+		announcement.copy(submittedAnnouncement)
+		announcement.save()
 
-        return announcement.toJson()
-    }
+		return announcement.toJson()
+	}
 }
