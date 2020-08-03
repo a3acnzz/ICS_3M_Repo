19b8c62a6a77f12edf1371f7982c3f97a9eac795
Rajkumar <jeevanraorajaram@gmail.com>
2020-08-03 12:05:29 +0530

    erfe ui latest with cr's

--- erfe-api-backup-from-local-14-7-2020/src/main/java/com/mmm/erfe/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.mmm.erfe.service;
-
-import com.mmm.erfe.domain.GlobalUser;
-import com.mmm.erfe.service.impl.ServiceResponse;
-
-public interface UserService {
-    ServiceResponse login(GlobalUser user);
-}
--- erfe-api-backup-from-local-14-7-2020/src/main/resources/data/hsqldb/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Greeting (text) VALUES ('Hello World!');
-INSERT INTO Greeting (text) VALUES ('Hola Mundo!');
-
-
-INSERT INTO TERFE_USER(prsn_id,user_pin_txt,person_nme) VALUES ('GET1001','USSFAAA','GET1001')
-INSERT INTO TERFE_USER(prsn_id,user_pin_txt,person_nme) VALUES ('GET1002','USSFAAB','GET1002')
